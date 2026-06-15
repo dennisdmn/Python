0154b4761a82a884bcf541c0895567a5c4c6242e
dennisdmn <34797928+dennisdmn@users.noreply.github.com>
2026-06-15 14:13:38 -0300

    Adiciona init do pacote python_operacional

---
+++ python_operacional/__init__.py
@@ -0,0 +1,13 @@
+"""Blocos operacionais reutilizaveis para scripts Python."""
+
+__all__ = [
+    'selecao_arquivos',
+    'inventario_pastas',
+    'validacao_layouts',
+    'colunas',
+    'leitura_excel_pandas',
+    'leitura_excel_polars',
+    'sqlite_cache',
+    'exportacao_excel',
+    'conciliacao',
+]
